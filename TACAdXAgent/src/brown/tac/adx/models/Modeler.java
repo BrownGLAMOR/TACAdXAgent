b11656f5db7baf735cf9ddb55b4bab3603f8f872
bhilliard <betsy@cs.brown.edu>
2013-05-24 14:42:11 -0400

    testing commit by Betsy

--- TACAdXAgent/src/brown/tac/adx/models/Modeler.java
+++ TACAdXAgent/src/brown/tac/adx/models/Modeler.java
@@ -3,8 +3,10 @@ package brown.tac.adx.models;
 import java.util.LinkedList;
 
 import brown.tac.adx.predictions.DailyPrediction;
-/*
+
+/**
  * Class contains a list of models and update them in order of dependencies
+ *  @author Veena Vignale
  */
 public class Modeler {
 	
